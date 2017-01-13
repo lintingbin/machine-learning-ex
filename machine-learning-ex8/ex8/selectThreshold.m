59680888e9ab06dbbafac2a6f1ccf3e39cfd268c
unknown <>
2017-01-14 00:59:08 +0800

    Submit machine-learning-ex8

---
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -0,0 +1,58 @@
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
+%outliers
+%   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval).
+%
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the F1 score of choosing epsilon as the
+    %               threshold and place the value in F1. The code at the
+    %               end of the loop will compare the F1 score for this
+    %               choice of epsilon and set it to be the best epsilon if
+    %               it is better than the current choice of epsilon.
+    %               
+    % Note: You can use predictions = (pval < epsilon) to get a binary vector
+    %       of 0's and 1's of the outlier predictions
+
+
+    predict_y = (pval < epsilon);
+    tp = sum(predict_y & yval);
+    predict_p = sum(predict_y);
+    true_p = sum(yval);
+
+    if predict_p > 0
+        prec = tp / predict_p;
+    else
+        prec = 0;
+    end
+
+    if true_p > 0
+        rec = tp / true_p;
+    else
+        rec = 0
+    end
+
+    if (prec + rec) > 0
+        F1 = 2 * prec * rec / (prec + rec);
+    else
+        F1 = 0;
+    end
+
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
